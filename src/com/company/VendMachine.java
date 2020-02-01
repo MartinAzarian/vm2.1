d7333b4b643e9f13ff42fb18084d2bce987c4c54
MartsLine <48711841+MartsLine@users.noreply.github.com>
2020-02-01 09:52:10 +0400

    Update VendMachine.java

--- src/com/company/VendMachine.java
+++ src/com/company/VendMachine.java
@@ -40,7 +40,7 @@ public class VendMachine {
         ArrayList<Product> candys = new ArrayList<>();
         ArrayList<Product> crisps = new ArrayList<>();
 
-        for(int i = 0; i < 10; i++){
+        for ( int i = 0; i < 10; i++ ) {
             
         Cola cola = new Cola();
         Pepsi pepsi = new Pepsi();
